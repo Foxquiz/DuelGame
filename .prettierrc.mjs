c8bd243966c84f4513815a4b6edb8e551d9c75df
Arina Bardyukova <arina.bardyukova@yandex.ru>
2024-08-21 21:38:03 +0300

    init

---
+++ .prettierrc.mjs
@@ -0,0 +1,5 @@
+export default {
+    semi: false,  // no semicolons
+    endOfLine: "lf",
+  };
+  
---
+++ eslint.config.js
@@ -0,0 +1,59 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import react from 'eslint-plugin-react'
+import perfectionist from 'eslint-plugin-perfectionist'
+
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+      perfectionist.configs['recommended-natural'],
+      eslintPluginPrettierRecommended,
+    ],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      react
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { "argsIgnorePattern": "^_", "destructuredArrayIgnorePattern": "^_" }
+      ],
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          type: 'natural',
+          order: 'asc',
+        }
+      ],
+      'linebreak-style': ['error', 'unix']
+    },
+  },
+);
